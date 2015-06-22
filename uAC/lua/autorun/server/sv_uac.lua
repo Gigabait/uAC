6ae0ff394e13a04a4a488825bc9bad7370a5f51d
ThatN00B <nopenopenopenope123@gmail.com>
2015-06-21 19:03:26 -0700

    Variable checking & Detour checking

--- uAC/lua/autorun/server/sv_uac.lua
+++ uAC/lua/autorun/server/sv_uac.lua
@@ -1,4 +1,5 @@
 util.AddNetworkString("uAC_Ban")
+util.AddNetworkString("uAC_CheckVar")
 
 uAC = {}
 
@@ -8,8 +9,24 @@ function uAC.Punish(ply, reason)
 	end
 end
 
+function uAC.CheckVar(ply, var, val)
+	if var != "sv_allowcslua" and var != "sv_cheats" then
+		return
+	end
+	
+	local sv_val = GetConVarString(var)
+	
+	if sv_val != val then
+		uAC.Punish(ply, "CVar Forcing")
+	end
+end
+
 net.Receive("uAC_Ban", function(len, ply)
 	uAC.Punish(ply, net.ReadString())
+end )
+
+net.Receive("uAC_CheckVar", function(len, ply)
+	uAC.CheckVar(ply, net.ReadString(), net.ReadString())
 end )
 
 print("[uAC] Serverside loaded.")
