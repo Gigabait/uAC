49845fcaa349423ea7f15ec19bc1e05253ec0d5e
ThatN00B <nopenopenopenope123@gmail.com>
2015-06-21 18:16:27 -0700

    GAMEMODE Hook Detector

--- uAC/lua/autorun/client/cl_uac.lua
+++ uAC/lua/autorun/client/cl_uac.lua
@@ -1,5 +1,7 @@
 local table = table.Copy(table)
 local net = table.Copy(net)
+local timer = table.Copy(timer)
+local debug = table.Copy(debug)
 
 local function Punish(reason)
 	net.Start("uAC_Ban")
@@ -7,7 +9,41 @@ local function Punish(reason)
 	net.SendToServer()
 end
 
-local goodModules = {
+local function RandomString()
+	local randstr = " "
+	local rand1 = math.random(6, 12)
+	
+	for i = 1, rand1 do
+		local rand2 = math.random(80, 160)
+		randstr = randstr .. tostring(rand2)
+	end
+	
+	return randstr
+end
+
+local function CheckHooks()
+	if GAMEMODE.Think then
+		if string.Explode("/", debug.getinfo(GAMEMODE.Think).source)[1] != "@gamemodes" then
+			Punish("GAMEMODE Hook Overriding")
+		end
+	end
+	
+	if GAMEMODE.HUDPaint then
+		if string.Explode("/", debug.getinfo(GAMEMODE.HUDPaint).source)[1] != "@gamemodes" then
+			Punish("GAMEMODE Hook Overriding")
+		end
+	end
+	
+	if GAMEMODE.CreateMove then
+		if string.Explode("/", debug.getinfo(GAMEMODE.CreateMove).source)[1] != "@gamemodes" then
+			Punish("GAMEMODE Hook Overriding")
+		end
+	end
+end
+
+timer.Create(RandomString(), 5, 0, CheckHooks)
+
+local GoodModules = {
 	"ai_schedule",
 	"ai_task",
 	"baseclass",
@@ -50,7 +86,7 @@ local goodModules = {
 local oldRequire = require
 
 local function newRequire(mod)
-	if !table.HasValue(goodModules, mod) then
+	if !table.HasValue(GoodModules, mod) then
 		Punish("Bad Module: " .. mod)
 		return
 	end
